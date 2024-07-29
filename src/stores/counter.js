7bd65d266c7704c15ff320529d5c8b2043f7a2be
Edwin Lin <edwinlin@uitc.com.tw>
2024-07-29 16:10:27 +0800

    feat 將pinia store持久化

--- src/stores/counter.js
+++ src/stores/counter.js
@@ -1,38 +1,42 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
-export const useUserStore = defineStore('user', () => {
-  const username = ref(null)
-  const password = ref(null)
-  const userList = [
-    {
-      name: 'Andy',
-      username: 'AAA',
-      password: 'aaa'
-    },
-    {
-      name: 'Bob',
-      username: 'BBB',
-      password: 'bbb'
-    },
-    {
-      name: 'Cindy',
-      username: 'CCC',
-      password: 'ccc'
+export const useUserStore = defineStore(
+  'user',
+  () => {
+    const username = ref(null)
+    const password = ref(null)
+    const userList = [
+      {
+        name: 'Andy',
+        username: 'AAA',
+        password: 'aaa'
+      },
+      {
+        name: 'Bob',
+        username: 'BBB',
+        password: 'bbb'
+      },
+      {
+        name: 'Cindy',
+        username: 'CCC',
+        password: 'ccc'
+      }
+    ]
+
+    const Loginuser = computed(() => {
+      const user = userList.find(
+        (check) => check.username == username.value && check.password == password.value
+      )
+      return user
+    })
+
+    function userlogin(un, pw) {
+      username.value = un
+      password.value = pw
     }
-  ]
 
-  const Loginuser = computed(() => {
-    const user = userList.find(
-      (check) => check.username == username.value && check.password == password.value
-    )
-    return user
-  })
-
-  function userlogin(un, pw) {
-    username.value = un
-    password.value = pw
-  }
-
-  return { Loginuser, userlogin }
-})
+    return { Loginuser, userlogin }
+  },
+  { persist: true }
+)
